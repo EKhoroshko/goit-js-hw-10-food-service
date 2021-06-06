59ff2470c1cab0f424210ea4dc7c5064eec57a17
EKhoroshko <xapoxa14@gmail.com>
2021-06-07 01:33:09 +0300

    full

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 import './sass/main.scss';
+import foodCards from './templation/foos-cards.hbs';
+import menu from './menu.json';
+
+const $menu = document.querySelector('.js-menu');
+$menu.insertAdjacentHTML('beforeend', foodCards(menu));
+
+const $input = document.querySelector('.theme-switch__toggle');
+$input.addEventListener('change', (e) => {
+    e.preventDefault();
+    if (localStorage.getItem('theme') === 'dark-theme') {
+        localStorage.removeItem('theme');
+    }
+    else {
+        localStorage.setItem('theme', 'dark-theme')
+    }
+    addDarkMode();
+});
+
+function addDarkMode() {
+    if (localStorage.getItem('theme') === 'dark-theme') {
+        document.querySelector('body').classList.add('dark-theme');
+    }
+    else {
+        document.querySelector('body').classList.remove('dark-theme');
+    }
+}
+addDarkMode();
